79200fac44dbe63ac56972ba45ea624995583210
Bruna Belini <brunabbelini1@hotmail.com>
2025-03-31 16:37:31 -0300

    Parte de login e autenticação

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib import auth
 
 # Create your views here.
 def cadastro(request):
@@ -32,3 +34,20 @@ def cadastro(request):
         )
 
         return redirect('/usuarios/login')
+    
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = authenticate(request, username=username, password=senha)
+
+        if user:
+            auth.login(request, user)
+            return redirect('/mentorados/')
+        
+        messages.add_message(request, constants.ERROR, 'Username ou senha inválidos.')
+        return redirect('login')
